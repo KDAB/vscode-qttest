9cc15b6e0c8385b6713e380af2d7c0f27623d8a6
Sergio Martins <sergio.martins@kdab.com>
2023-03-29 21:48:35 +0100

    Added an example test project
    Easier to test and debug

---
+++ test/qt_test/test1.cpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group
+// company <info@kdab.com> Author: Sergio Martins <sergio.martins@kdab.com>
+// SPDX-License-Identifier: MIT
+
+#include <QObject>
+#include <QtTest>
+
+class MyTest : public QObject {
+  Q_OBJECT
+private Q_SLOTS:
+  void test1() {}
+  void test2() {}
+  void test3() {}
+};
+
+QTEST_MAIN(MyTest);
+
+#include <test1.moc>
