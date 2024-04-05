d5b8cedff179143c5ba1416604658c6caa696b71
Sergio Martins <sergio.martins@kdab.com>
2024-04-05 20:17:34 +0100

    test: Add integration tests
    Just a dummy for now

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,13 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+suite('Extension Test Suite', () => {
+    suiteTeardown(() => {
+        vscode.window.showInformationMessage('All tests done!');
+    });
+
+    test('Sample test', () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+});
