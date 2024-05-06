a3619fe453fca48d892fd7667653aca55da6310f
Sergio Martins <sergio.martins@kdab.com>
2024-05-06 14:53:05 +0100

    ci: package workflow now adds the package as asset

---
+++ upload_package.sh
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# Author: Sergio Martins <sergio.martins@kdab.com>
+# SPDX-License-Identifier: MIT
+
+# Uploads the qttests-<version>.vsix package to GitHub Release
+# Called by .github/workflows/package.yml
+
+# Get latest tag:
+TAG_NAME=`git describe --tags --abbrev=0`
+
+# Drop the 'v' prefix:
+VERSION=${TAG_NAME:1}
+
+PACKAGE_FILENAME=qttests-$VERSION.vsix
+
+if [ ! -f $PACKAGE_FILENAME ]; then
+    # Doesn't happen. Package is created by package.yml
+    echo "Package $PACKAGE_FILENAME does not exist"
+    exit 1
+fi
+
+# Check if release exists:
+gh release view $TAG_NAME &> /dev/null
+if [ $? -ne 0 ]; then
+    # Should not happen, as releases are created by release-please
+    echo "Release $TAG_NAME does not exist"
+    exit 1
+fi
+
+# Check if release already contains the asset:
+gh release view $TAG_NAME --json assets | jq -r '.assets[].name' | grep -q $PACKAGE_FILENAME
+
+if [ $? -eq 0 ]; then
+    echo "Asset $PACKAGE_FILENAME already exists in release $TAG_NAME"
+    exit 0
+fi
+
+gh release upload $TAG_NAME $PACKAGE_FILENAME
